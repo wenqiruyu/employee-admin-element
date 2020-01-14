773480ddb116de7d7b082e5212258aba522d5fa3
Allen <yingxing.liu1@cignacmb.com>
2020-01-14 18:36:54 +0800

    提价代码

--- src/components/page/deptList.vue
+++ src/components/page/deptList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div>
-      <el-tree :data="deptList" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    <div class="dept-tree">
+      <el-tree :data="deptList" node-key="deptName" :default-expanded-keys="['公司组织架构']" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
     </div>
   </div>
 </template>
@@ -11,6 +11,9 @@
         name: "deptList",
         data() {
             return {
+                conheight: {
+                    height:''
+                },
                 deptList: [{
                     deptName: '公司组织架构',
                     children: []
@@ -22,19 +25,35 @@
             };
         },
         created() {
+            window.addEventListener('resize', this.getHeight);
+            this.getHeight()
             this.getAllDept();
         },
         methods: {
+            getHeight(){
+                this.conheight.height=window.innerHeight-170+'px';
+            },
             getAllDept(){
                 this.$axios.post("/employee-admin-server/dept/getAllDept").then((res) => {
                     if (res.data.code == 200) {
+                        for (let i = 0; i < res.data.data.length; i++) {
+                            this.deptList[0].children.push({
+                                deptId: res.data.data[i].deptId,
+                                deptName: res.data.data[i].deptName,
+                                deptDescription: res.data.data[i].deptDescription,
+                                principalEmpId: res.data.data[i].principalEmpId,
+                                principalName: res.data.data[i].principalName,
+                                children: []
+                            })
+                            for(let j = 0; j < res.data.data[i].position.length; j++){
+                                this.deptList[0].children[i].children.push({
+                                    deptName: res.data.data[i].position[j].positionName,
+                                    positionDesc: res.data.data[i].position[j].positionDesc
+                                })
+                            }
+                        }
                         // 数组复制
-                        this.deptList[0].children = res.data.data.slice()
-                        console.log(res.data.data)
-                        this.$message({
-                            message: '恭喜你，成功获取员工信息',
-                            type: 'success'
-                        })
+                        // this.deptList[0].children = res.data.data.slice()
                     } else {
                         this.$message.error(res.data.msg)
                     }
@@ -47,6 +66,13 @@
     }
 </script>
 
-<style scoped>
-
+<style>
+  .dept-tree{
+    width: 300px;
+    height: 100%;
+    overflow:hidden;
+  }
+  .el-tree{
+    height: 100%;
+  }
 </style>
